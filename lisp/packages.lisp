aa977885818d45687415ada0424a5f1c2e3ad02e
Christian Schafmeister <meister@temple.edu>
2020-11-29 18:49:47 -0500

    Export enter-full-screen symbol

--- lisp/packages.lisp
+++ lisp/packages.lisp
@@ -1,8 +1,9 @@
 (defpackage #:resizable-box
   (:use #:common-lisp)
   (:export
-    #:full-screen
-    #:resizable-grid-box
-    #:resizable-layout
-    #:resize))
+   #:full-screen
+   #:enter-full-screen
+   #:resizable-grid-box
+   #:resizable-layout
+   #:resize))
 
